562cbb65799881744e4ee8c8824378f314dd947c
Julian Ammann <julian.ammann@dkd.de>
2022-12-05 09:18:56 +0100

    remove )

--- src/main/resources/META-INF/init.sql
+++ src/main/resources/META-INF/init.sql
@@ -4,4 +4,4 @@ INSERT INTO Reporter (firstname, lastname, phonenumber) VALUES ('Admiral', 'Ackb
 INSERT INTO GhostNet (latitude, longitude, estimatedSize, state, ghostnetstateenum, rescuer_id, reporter_id) VALUES (45.821044, -10.737615, '2000cm', 'reported', 'Reported', null, null)
 INSERT INTO GhostNet (latitude, longitude, estimatedSize, state, ghostnetstateenum, rescuer_id, reporter_id) VALUES (44.872751, -15.916663, '2200cm', 'recovered', 'Recovered', 1, null)
 INSERT INTO GhostNet (latitude, longitude, estimatedSize, state, ghostnetstateenum, rescuer_id, reporter_id) VALUES (50.248277, 0.289105, '1000cm', 'lost', 'Lost', 2, 1)
-INSERT INTO GhostNet (latitude, longitude, estimatedSize, state, ghostnetstateenum, rescuer_id, reporter_id) VALUES (39.302288, 5.865443, '9000cm', 'recoveryImminent', 'RecoveryImminent', 1, null))
+INSERT INTO GhostNet (latitude, longitude, estimatedSize, state, ghostnetstateenum, rescuer_id, reporter_id) VALUES (39.302288, 5.865443, '9000cm', 'recoveryImminent', 'RecoveryImminent', 1, null)
